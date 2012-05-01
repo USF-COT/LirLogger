8a5946323db213b79484e1c34ef316a6ec0f4c5c
Michael Lindemuth <mlindemu@cotprojects.marine.usf.edu>
2012-05-01 14:13:27 +0000

    Boost threading works with Spyder3Camera class.  Will use this as the model for all sensor readers.

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ EXEC     = LirLogger
 #CFLAGS+=$(shell pkg-config  --cflags $(FFMPEG_LIBS))
 #CPPFLAGS+=$(shell pkg-config  --cflags $(FFMPEG_LIBS))
 #LDFLAGS+=$(shell pkg-config --libs $(FFMPEG_LIBS))
-LDFLAGS+=-ltiff -lxerces-c -L/usr/src/boost_1_49_0/bin.v2/libs/thread/build/gcc-4.1.2/release/address-model-64/threading-multi/ -lboost_thread
+LDFLAGS+=-ltiff -lxerces-c -lboost_thread
 CPPFLAGS+=-D__STDC_CONSTANT_MACROS -I/usr/src/boost_1_49_0
 
 include sample.Makefile
--- Spyder3Camera.cpp
+++ Spyder3Camera.cpp
@@ -18,14 +18,17 @@
 Spyder3Camera::Spyder3Camera(const char* _MAC, unsigned int _pipelineBufferMax) : pipelineBufferMax(_pipelineBufferMax){
     MAC = (char*) malloc(sizeof(char)*(strlen(_MAC)+1));
     strncpy(MAC,_MAC,strlen(_MAC)+1);
+    camThread = NULL;
 }
 
 Spyder3Camera::~Spyder3Camera(){
+    free(MAC);
 }
 
 bool Spyder3Camera::start(){
     if(camThread == NULL){
         isRunning = true;
+        syslog(LOG_DAEMON|LOG_INFO,"Starting camera thread.");
         camThread = new boost::thread(boost::ref(*this));
     }
 }
@@ -36,17 +39,27 @@ bool Spyder3Camera::stop(){
         isRunning = false;
         runMutex.unlock();
         
+        syslog(LOG_DAEMON|LOG_INFO,"Joining camera thread.");
         if(camThread) camThread->join();
         camThread = NULL;
     }
 }
 
 void Spyder3Camera::operator() (){
-   // Setup code
-    syslog(LOG_DAEMON|LOG_INFO,"Camera started.");
+    bool running;
+
+    // Setup code
+    syslog(LOG_DAEMON|LOG_INFO,"Camera thread running.");
 
     // Pipeline loop 
+    do{
+
+        runMutex.lock();
+        running = isRunning;
+        runMutex.unlock();
+
+    }while(running); 
 
     // Housekeeping!
-    syslog(LOG_DAEMON|LOG_INFO,"Camera stopped."); 
+    syslog(LOG_DAEMON|LOG_INFO,"Camera thread stopped.");
 }
