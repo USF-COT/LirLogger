709f571693903f0e59d72f8773ad8352240246ef
Michael Lindemuth <mlindemu@usf.edu>
2013-06-07 22:06:34 +0000

    ZMQ server running on 5555 and forwards all requests to LirCommand

--- LirLogger.cpp
+++ LirLogger.cpp
@@ -14,20 +14,14 @@
 #include <stdlib.h>
 #include <syslog.h>
 #include <unistd.h>
-#include <errno.h>
 #include <signal.h>
-#include <string.h>
 
 #include "LirCommand.hpp"
-#include "Spyder3Camera.hpp"
-#include "LirTCPServer.hpp"
 
 volatile sig_atomic_t daemonrunning=1;
-boost::asio::io_service io_service;
 
 void catch_term(int sig){
     daemonrunning=0;
-    io_service.stop();
     return;
 }
 
@@ -36,11 +30,13 @@ int main(){
 
     openlog("LIRLOGGER",LOG_PID,0);
 
+    /*
     if(daemon(1,0)){
         syslog(LOG_DAEMON|LOG_ERR,"Error becoming daemon.  Error: %d  Exiting.",errno);
         exit(EXIT_FAILURE);
     }
     syslog(LOG_DAEMON|LOG_INFO,"Daemon running");
+    */
 
     // Initialize Lir Command 
     syslog(LOG_DAEMON|LOG_INFO,"Creating command instance.");
@@ -55,13 +51,22 @@ int main(){
     socket.bind("tcp://*:5555");
 
     while(daemonrunning){
-        zmq::message_t request;
+        zmq::message_t requestMessage;
 
-        socket.recv(&request);
+        // Receive Request
+        socket.recv(&requestMessage);
+        std::string request((char *)requestMessage.data(), requestMessage.size());
+        std::string response = com->parseCommand(request);
+
+        // Send Response
+        zmq::message_t responseMessage(response.size());
+        memcpy((void*)responseMessage.data(), response.data(), response.size());
+        socket.send(responseMessage);
     }
 
     // Housekeeping
     com->stopLogger(); // Just in case it was left running
+    socket.close();
     syslog(LOG_DAEMON|LOG_INFO,"Daemon Exited Successfully.");
     closelog();
     exit(EXIT_SUCCESS);
